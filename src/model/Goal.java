227fffaf9158ff60c53f446ce61290b3f5d00f96
ZanottiMatteo <m.zanotti13@studenti.unibg.it>
2023-05-08 19:00:00 +0200

    Creazione estensioni Goal

--- src/model/Goal.java
+++ src/model/Goal.java
@@ -4,9 +4,10 @@ public abstract class Goal {
 	private int points;
 	
 	public void addPoint(int player, int points) {
-		
+		this.points=this.points+points;
 	}
 	
-	public void printPoint() {
+	public void printPoint(int player, int points) {
+		System.out.println("Player: " + player + "Point: " + points);
 	}
 }
