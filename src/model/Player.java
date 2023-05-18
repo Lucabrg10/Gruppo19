c1916876116281f26775c41f97974d1830ce605e
mstefi <127400167+mstefi@users.noreply.github.com>
2023-05-18 14:49:01 +0200

    classe player

---
+++ src/model/Player.java
@@ -0,0 +1,23 @@
+package model;
+import java.util.Scanner;
+
+public class Player {
+	
+	
+	private final int idPlayer;
+	private String playerName;
+	private int count=0;
+	
+	public Player(String name){
+		this.idPlayer=count;
+		count++;
+		
+		this.playerName=name;
+		
+		
+		
+	}
+
+	
+	
+}
