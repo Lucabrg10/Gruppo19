c8c07fa0dac86983001d95086b2b7b1f355cb693
mstefi <127400167+mstefi@users.noreply.github.com>
2023-05-23 16:18:36 +0200

    player

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,5 @@
 package model;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Player {
@@ -7,8 +8,12 @@ public class Player {
 	private final int idPlayer;
 	private String playerName;
 	private Shelf playerShelf;
-	
-	
+	private final int[][]Direction= {
+			{1,0},
+			{-1,0},
+			{0,1},
+			{0,-1}
+	};
 	public Player(String name, int count){
 		this.idPlayer=count;
 		
@@ -30,6 +35,17 @@ public class Player {
 		
 		
 		return 0;
+	}
+	
+	public void nearTiles(ColorTile [][]matrix, int i, int j, ColorTile card, HashMap<Integer, Integer> coordinate) {
+		
+		if(i<0 || j<0 || i>=matrix.length || j>=matrix[0].length) {
+			return;
+		}
+		if(!matrix[i][j].equals(card)) {
+			return;
+		}
+		
 	}
 	
 }
